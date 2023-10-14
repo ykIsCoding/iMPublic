f7267e59df461ec288e214a64e47172617d86ffa
Quinn Heffern <quinnheffern@quinns-mbp.mynetworksettings.com>
2023-10-14 11:34:43 -0400

    feat: I have the different appsettings working and the appsettings.{EnvironmentName}.json ignored.

--- InvolveMINT.API/InvolveMINT.API/Program.cs
+++ InvolveMINT.API/InvolveMINT.API/Program.cs
@@ -10,9 +10,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Configuration
-  .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-  .AddEnvironmentVariables();
+// builder.Configuration
+//   .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+//   .AddJsonFile("appsettings.{env.Environment}.json", optional: true, reloadOnChange: true)
+//   .AddEnvironmentVariables();
 
 builder.Services.AddControllers();
 
