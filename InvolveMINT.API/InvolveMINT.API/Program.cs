f56654f3c6b90ec9a6a7826a858e54ce4018107b
Quinn Heffern <quinnheffern@quinns-mbp.mynetworksettings.com>
2023-10-14 11:36:49 -0400

    Cleaned some dead code.

--- InvolveMINT.API/InvolveMINT.API/Program.cs
+++ InvolveMINT.API/InvolveMINT.API/Program.cs
@@ -9,12 +9,6 @@ using InvolveMINT.API.Core.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-// builder.Configuration
-//   .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-//   .AddJsonFile("appsettings.{env.Environment}.json", optional: true, reloadOnChange: true)
-//   .AddEnvironmentVariables();
-
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
